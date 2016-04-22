a7146abe5c16e455a39752acd28a32c6b18fa208
Rye Nguyễn <Rye Nguyn>
2016-04-22 13:13:27 +0700

    - Add abstract function (complete for reaction solver)

--- projects/MainForm.cs
+++ projects/MainForm.cs
@@ -12,20 +12,6 @@ namespace ReactionSeriesSolver
 {
     public partial class MainForm : Form
     {
-		List<string> m_elementsLeft;
-		public List<string> ElementLeft
-		{
-			get { return m_elementsLeft; }
-			set { m_elementsLeft = value; }
-		}
-
-		List<string> m_elementsRight;
-		public List<string> ElementRight
-		{
-			get { return m_elementsRight; }
-			set { m_elementsRight = value; }
-		}
-
 		List<Pair<List<string>, List<string>>> m_reactionList;
 		public List<Pair<List<string>, List<string>>> ReactionList
 		{
@@ -41,8 +27,20 @@ namespace ReactionSeriesSolver
 
 		private void btn_solve_reaction_Click(object sender, EventArgs e)
 		{
-			string _reaction = txt_reaction.Text;
-			Processor.AnalyzeReaction(ElementLeft, ElementRight, _reaction);
+			string _reactionStr = txt_reaction.Text;
+			Pair<List<string>, List<string>> _reaction = Processor.AnalyzeReaction(_reactionStr);
+
+			Pair<List<string>, List<string>> _reactionTarget = Processor.FindReaction(ReactionList, _reaction);
+
+			if(_reactionTarget != null)
+			{
+				List<int> _coefficients = Processor.BalanceReaction(_reactionTarget);
+				txt_result_reaction.Text = Processor.GenerateReaction(_reactionTarget, _coefficients);
+			}
+			else
+			{
+				txt_result_reaction.Text = "Could not find reaction!!!";
+			}
 		}
 
 		private void btn_solve_reactionSeries_Click(object sender, EventArgs e)
--- projects/Processor.cs
+++ projects/Processor.cs
@@ -56,19 +56,55 @@ namespace ReactionSeriesSolver
 			return new Pair<List<string>, List<string>>(_first, _second);
 		}
 
-		public static void AnalyzeReaction(List<string> left, List<string> right, string reaction)
+		public static Pair<List<string>, List<string>> AnalyzeReaction(string reaction)
 		{
 			// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
 			// FOR DEBUG
 			// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
-			left.Add("Cl2");
-			right.Add("NaCl");
-			right.Add("NaClO");
+			List<string> _first = new List<string>();
+			List<string> _second = new List<string>();
+
+			_first.Add("Cl2");
+
+			_second.Add("NaCl");
+			_second.Add("NaClO");
+
+			return new Pair<List<string>, List<string>>(_first, _second);
 
 			// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
 			// FOR RELEASE
 			// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
 			// TODO: Add real algorithm
+		}
+
+		public static Pair<List<string>, List<string>> FindReaction(List<Pair<List<string>, List<string>>> reactionList, 
+																	Pair<List<string>, List<string>> reaction)
+		{
+			// TODO: Add default value: null
+			return reactionList[0];
+		}
+
+		public static List<int> BalanceReaction(Pair<List<string>, List<string>> reaction)
+		{
+			List<int> _coefficients = new List<int>();
+
+			_coefficients.Add(1);
+			_coefficients.Add(2);
+			_coefficients.Add(3);
+			_coefficients.Add(4);
+			_coefficients.Add(5);
+
+			return null;
+		}
+
+		public static string GenerateReaction(Pair<List<string>, List<string>> reaction, List<int> coefficients)
+		{
+			//foreach (string _reactant in reaction.First)
+			//{
+
+			//}
+
+			return "Cl2 + NaOH -> NaCl + NaClO + H2O";
 		}
 	}
 }
